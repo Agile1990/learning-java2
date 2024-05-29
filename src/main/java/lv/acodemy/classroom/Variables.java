92631bc2a1102416e08af4ca031369859d5aca53
Agile1990 <Agi1e@inbox.lv>
2024-05-29 23:36:34 +0300

    [classroom/290524]: Add example with operations

--- src/main/java/lv/acodemy/classroom/Variables.java
+++ src/main/java/lv/acodemy/classroom/Variables.java
@@ -4,38 +4,47 @@ public class Variables {
     public static void main(String[] args) {
 
         // One-line comment
-        /* Multi-line comment
 
+        /*
+        Multi-line comment
+        I can also write here
          */
 
         // Category: Integer
 
         //Byte:
         byte smallNumber = 120;
+
         // RGB (Red Green Blue)
         byte red = 120;
         byte green = 45;
         byte blue = 127;
         System.out.println(blue);
 
-        //Int - (Integer)
+        // Int - (Integer)
+        // Employee ID:
         int employId = 123456;
-        //Population count:
+
+        // Population count:
         int populationOfLatvia = 1811956;
         System.out.println(populationOfLatvia);
 
-        //short
-        //Game Score
+        // short
+        // Game Score
         short playerScore = 15000;
 
         // Year of birth
         short yearOfBirth = 1990;
 
-        //char (Character)
+        // char (Character)
+        // Grade:
+        char grade = 'A';
+
+        // Initials:
         char firstInitial = 'A';
         char lastInitial = 'T';
 
-        //long
+        // long
         // Bank Account Balance
         long accountBalance = 1000000L;
         System.out.println(accountBalance);
@@ -44,31 +53,35 @@ public class Variables {
         // Improves readability
         long population = 1_000_000_000L; //1 billion
 
-        //Avoiding mistake
+        // Avoiding mistake
         int creditCardNumber = 1234_5678;
 
-        //double
+        // double
         // PI
-
         double piValue = Math.PI;
         System.out.println(piValue);
 
+        // Another way
+        double pi = 3.14_15_92;
+
         // Floating Number
-        //Price
+        // Float
+        // Price
         float price = 1_234_567_8998_98F;
 
-        double financeDebit = 23_456_789_45555;
+        // Double
+        // Financial Debt:
+        double financeDebit = 23_456_789_123.45555;
 
         //Distance
-
         double distanceToSun = 149_549_870_3; //in km
 
-        //Boolian
-        // has or is prefix
+        // Boolean (true / false)
+        // has or is prefix;
 
-        boolian isSummer = false;
-        boolian hasSon = false;
-        boolian hasBMW = true;
+        boolean isSummer = false;
+        boolean hasSon = false;
+        boolean hasBMW = true;
 
         // String - we can save here any text in double quotes
 
@@ -79,18 +92,30 @@ public class Variables {
         var myCurrentAge = 33;
         var myFullName = "Aleksandrs Taranenko";
 
-
-        //Operators
-        // * - multiplication (8*8)
-        // / - division (8/2)
-
-
-        boolian evenNumber = 4 % 2 == 0; //true
-        boolian oddNumber = 4 % 3 == 0 // false
+        // Operators:
+        // * - multiplication (8 * 8)
+        // / - division (8 / 2)
+        // + - Addition (5 + 5)
+        // - - Subtraction (10 - 4)
+        // --  (-1)
+        //  ++ (+1)
+        // % (the remainder after dividing a by b)
+        
+        boolean evenNumber = 4 % 2 == 0; //true
+        boolean oddNumber = 4 % 3 == 0; // false
 
         int remainderResult = 5 % 3;
         System.out.println(remainderResult);
 
+        // Operators (Simple example)
+        int x = 10;
+        int y = 20;
+        int result = x + y;
 
+        int a = 30;
+        int b = 40;
+        int anotherResult = a - b;
+
+        int c = 10 + 20;
     }
 }
